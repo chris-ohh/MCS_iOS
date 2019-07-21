26ea12bf75e4c2607f09a2eae24458b8e8f5ddf7
Chris <chrisoh1721@gmail.com>
2019-07-21 18:36:07 -0400

    image in detailview

--- Week2Test/Week2Test/DetailViewController.swift
+++ Week2Test/Week2Test/DetailViewController.swift
@@ -22,18 +22,43 @@ class DetailViewController: UIViewController {
   
   @IBOutlet weak var summaryTextView: UITextView!
   
+  @IBOutlet weak var imageView: UIImageView!
   
   var episode: Episode?
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    episodeTitleLabel.text = "Episode Title: \(episode!.name)"
-    airDateLabel.text = "Premier Date: \(episode!.airdate)"
-    airTimeLabel.text = "Airtime: \(episode!.airtime)"
-    seasonLabel.text = "Season: \(episode!.season)"
-    episodeNumberLabel.text = "Episode Number: \(episode!.number)"
-    summaryTextView.text = "Summary: \(String((episode?.summary.dropLast(4).dropFirst(3))!))"
+    guard let episode = episode else { return }
+    let url = episode.image.original
+    
+    URLSession.shared.dataTask(with: url) { data, response, error in
+      guard
+        let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+        let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+        let data = data, error == nil,
+        let image = UIImage(data: data)
+        else { return }
+      
+      DispatchQueue.main.async() {
+        self.episodeTitleLabel.text = "Episode Title: \(episode.name)"
+        self.airDateLabel.text = "Premier Date: \(episode.airdate)"
+        self.airTimeLabel.text = "Airtime: \(episode.airtime)"
+        self.seasonLabel.text = "Season: \(episode.season)"
+        self.episodeNumberLabel.text = "Episode Number: \(episode.number)"
+        self.summaryTextView.text = "Summary: \(String((episode.summary.dropLast(4).dropFirst(3))))"
+        self.imageView.image = image
+      }
+      }.resume()
+    
+    
+//    episodeTitleLabel.text = "Episode Title: \(episode!.name)"
+//    airDateLabel.text = "Premier Date: \(episode!.airdate)"
+//    airTimeLabel.text = "Airtime: \(episode!.airtime)"
+//    seasonLabel.text = "Season: \(episode!.season)"
+//    episodeNumberLabel.text = "Episode Number: \(episode!.number)"
+//    summaryTextView.text = "Summary: \(String((episode?.summary.dropLast(4).dropFirst(3))!))"
+//    imageView.image = episode!.image.medium
   
   }
   
