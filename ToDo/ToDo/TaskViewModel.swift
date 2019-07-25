d7ef730f0f511fb16f6e24ec1328c632ecb45ae2
Chris <chrisoh1721@gmail.com>
2019-07-24 22:38:44 -0400

    task view model

--- ToDo/ToDo/TaskViewModel.swift
+++ ToDo/ToDo/TaskViewModel.swift
@@ -1,17 +1,17 @@
 //
-//  Task+CoreDataProperties.swift
+//  TaskViewModel.swift
 //  ToDo
 //
-//  Created by MCS on 7/19/19.
+//  Created by MCS on 7/24/19.
 //  Copyright © 2019 MCS. All rights reserved.
 //
-
 
 import Foundation
 import CoreData
 
 
-extension Task {
+@objc(Task)
+public class Task: NSManagedObject {
   
   @nonobjc public class func fetchRequest() -> NSFetchRequest<Task> {
     return NSFetchRequest<Task>(entityName: "Task")
